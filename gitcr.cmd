e2729033d5e0b8e6d1fdd9f658ad972d8a951126
LA004 <anton.luette@franke.com>
2019-11-06 16:55:27 +0100

    first commit

---
+++ gitcr.cmd
@@ -0,0 +1,6 @@
+echo "# address-manager" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:S0010799879/address-manager.git
+git push -u origin master
